d5bb8ed59c68de1deec31cd7ec97c0c6917e9312
Khadijah Wali <kwali2@umbc.edu>
2016-11-11 16:55:46 -0500

    Add files via upload
    Edited homescreen so it links directly to the pages it was originally redirected to from processHome.php. I also changed the pre-advising worksheet button to link to the UMBC pre-advising worksheet pdf

--- studentSide/homescreen.php
+++ studentSide/homescreen.php
@@ -12,13 +12,31 @@ input[type=submit]{
 </style>
 </head>
 <body>
-<form>
-<form action='processHome.php' method='post' name='studemtHome'>
-<input type='submit' name='next' value='Search for An Appointment'><br/><br/>
-<input type='submit' name='next' value='View My Appointment'><br/><br/>
-<input type='submit' name='next' value='Pre-Advising Worksheet'><br/><br/>
-<input type='submit' name='next' value='Edit My Information'><br/><br/>
-<input type='submit' name='next' value='Logout'><br/>
+
+
+<form action='searchAppointments.php' method='post' name='studentHome'>
+<input type='submit' name='next' value="Search for An Appointment"><p>
 </form>
+
+
+<form action='viewAppointment.php' method='post' name='studentHome'>
+<input type='submit' name='next' value="View My Appointment"><p>
+</form>
+
+
+<form action='http://my.umbc.edu/system/shared/attachments/7d0dce8697b14cac5a02533f01ed201d/58263b8e/group-documents/000/002/686/4a300d3a0ae99b58b0dfcd3fde526bf5/Pre-Registration%20Sheet-NEW%202012.pdf?1349196261' method='post' name='studentHome'>
+<input type='submit' name='next' value="Get Pre-Advising Worksheet"><p>
+</form>
+
+
+<form action='Edit My Information' method='post' name='studentHome'>
+<input type='submit' name='next' value="Edit My Information"><p>
+</form>
+
+
+<form action='logout.php' method='post' name='studentHome'>
+<input type='submit' name='next' value="Logout"><p>
+</form>
+
 </body>
 </html>
