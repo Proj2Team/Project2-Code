d35798e828d58a91cce9776bf21d6634b6305712
linstephen13 <lin14@umbc.edu>
2016-12-02 13:52:08 -0500

    minor tweaks for creating appointment

--- advisorSide/createNewAppt.php
+++ advisorSide/createNewAppt.php
@@ -14,8 +14,8 @@ date_default_timezone_set('EST');
 	  <fieldset class='group'>
 	    <legend><caption><label for='selectedDate'> Creating Appointment for <?php echo date("l Y-m-d", strtotime($date)); ?> </label></caption></legend>
 	    <input name='selectedDate' type='hidden' value='<?php echo $date; ?>'/>
-	    <span>Select Appointment Start Time (Hour - Minute - AM/PM): </span><input type="time" name="start_time"><br/>
-	    <span>Select Appointment End Time (Hour - Minute - AM/PM): </span><input type="time" name="end_time"><br/>
+	    <span>Select Appointment Start Time (Hour:Minute AM/PM): </span><input type="time" name="start_time"><br/>
+	    <span>Select Appointment End Time (Hour:Minute AM/PM): </span><input type="time" name="end_time"><br/>
 	    <span>Number of Students Capacity (1-40): </span><input type="number" name="numStudents" min="1" max="40" value="1"><br/>
 	    <span>Location (Optional): </span><input type="text" name="location"><br/>
 	    <span>Session Type: </span><br/>
@@ -23,10 +23,20 @@ date_default_timezone_set('EST');
 	    <input type="radio" name="session_type" value="Individul"> Individual <br/>
 	    <span>Session Leader: </span><br/>
 	    <select name="session_leader">
-	      <option value="Michelle Bulger" selected>Ms. Michelle Bulger</option>
-	      <option value="Julie Crosby">Mrs. Julie Crosby</option>
-	      <option value="Christine Powers">Ms. Christine Powers</option>
-	      <option value="CNMS Advisors">CNMS Advisors</option>
+	      <?php
+	      	// Generate all options based on array of advisors, so that it is easily changed
+	      	// $advisors is echoed in the value attribute
+	      	$advisors = array("Michelle Bulger", "Julie Crosby", "Christine Powers", "CNMS Advisors");
+	      	// $advisor name is echoed in the actual drop down, aka what the user will see
+	      	$advisor_name = array("Ms. Michelle Bulger", "Mrs. Julie Crosby", "Ms. Christine Powers", "CNMS Advisors");
+	      	 $i = 0; ?>
+	      	<option value="<?php echo $advisors[$i]; ?>" selected><?php echo $advisor_name[$i]; ?></option>
+	      	<?php $i++;
+	      	while ($i < sizeof($advisors)) { ?>
+				<option value="<?php echo $advisors[$i]; ?>"><?php echo $advisor_name[$i]; ?></option>
+			<?php $i++;
+			}
+	      ?>
 	    </select><br/>
 	    <input class="button" type='submit' value='Save Appointments'>
 	  </fieldset><br/><br/>
